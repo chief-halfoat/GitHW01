281932fd8425de7fb65f9e8fb56c011d5875f715
chief-halfoat <johnathan.j.hart@gmail.com>
2022-05-15 10:02:09 -0400

    second commit

--- src/HW1.java
+++ src/HW1.java
@@ -1,6 +1,6 @@
 public class HW1 {
     public static void main(String[] args) {
-        for (int i = 1; i <=100; i++) {
+        for (int i = 1; i <=50; i++) {
             System.out.print(i+" ");
         }
     }
